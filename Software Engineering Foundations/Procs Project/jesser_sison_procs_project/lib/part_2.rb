b1a9affd119ec739042f684bf102c1bdd8e2fe7e
Jesser Sison <jesser.sison@live.com>
2019-05-29 00:26:27 -0700

    [05/29/19 00:26] Procs Project Part 2

--- Software Engineering Foundations/Procs Project/jesser_sison_procs_project/lib/part_2.rb
+++ Software Engineering Foundations/Procs Project/jesser_sison_procs_project/lib/part_2.rb
@@ -1,19 +1,19 @@
 def reverser(str, &proc)
-
+    proc.call(str.reverse)
 end
 
 def word_changer(str, &proc)
-
+    str.split().collect { |word| proc.call(word) }.join(" ")
 end
 
 def greater_proc_value(num, proc1, proc2)
-
+    proc1.call(num) >= proc2.call(num) ? proc1.call(num) : proc2.call(num)
 end
 
 def and_selector(arr, proc1, proc2)
-
+    arr.select { |item| proc1.call(item) && proc2.call(item) }
 end
 
 def alternating_mapper(arr, proc1, proc2)
-    
+    (0..arr.length - 1).collect { |i| i.even? ? proc1.call(arr[i]) : proc2.call(arr[i]) }
 end
